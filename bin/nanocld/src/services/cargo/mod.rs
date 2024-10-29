843b8513f324654ce21d201c42f3054737710696
leon3s <leo.vernisse@gmail.com>
2024-10-29 09:01:59 +0100

    tests/nanocld: cargo ssl secret

--- bin/nanocld/src/services/cargo/mod.rs
+++ bin/nanocld/src/services/cargo/mod.rs
@@ -35,15 +35,17 @@ pub fn ntex_config(config: &mut web::ServiceConfig) {
 #[cfg(test)]
 mod tests {
   use futures::{StreamExt, TryStreamExt};
-  use nanocl_stubs::system::{
-    EventActorKind, EventCondition, EventKind, NativeEventAction,
-  };
   use ntex::http;
 
-  use nanocl_stubs::cargo::{
-    Cargo, CargoDeleteQuery, CargoInspect, CargoKillOptions, CargoSummary,
+  use nanocl_stubs::{
+    cargo::{
+      Cargo, CargoDeleteQuery, CargoInspect, CargoKillOptions, CargoSummary,
+    },
+    cargo_spec::{CargoSpec, CargoSpecPartial},
+    proxy::ProxySslConfig,
+    secret::SecretPartial,
+    system::{EventActorKind, EventCondition, EventKind, NativeEventAction},
   };
-  use nanocl_stubs::cargo_spec::{CargoSpec, CargoSpecPartial};
 
   use crate::utils::tests::*;
 
@@ -293,6 +295,106 @@ mod tests {
       http::StatusCode::ACCEPTED,
       "init cargo delete"
     );
+    ntex::time::sleep(std::time::Duration::from_secs(1)).await;
+    system.state.wait_event_loop().await;
+  }
+
+  #[ntex::test]
+  async fn test_ssl() {
+    let system = gen_default_test_system().await;
+    let client = system.client;
+    let res = client
+      .send_post(
+        "/secrets",
+        Some(SecretPartial {
+          name: "test-secret-ssl".to_owned(),
+          kind: "nanocl.io/tls".to_owned(),
+          immutable: false,
+          // Some random metadata
+          metadata: Some(serde_json::json!({
+            "nginx": "ssl",
+          })),
+          data: serde_json::to_value(&ProxySslConfig {
+            certificate: "test random data".to_owned(),
+            certificate_key: "test random data".to_owned(),
+            certificate_client: Some("test random data".to_owned()),
+            verify_client: None,
+            dhparam: None,
+          })
+          .unwrap(),
+        }),
+        None::<String>,
+      )
+      .await;
+    test_status_code!(res.status(), http::StatusCode::CREATED, "create secret");
+    // create a cargo using the secret
+    let res = client
+      .send_post(
+        ENDPOINT,
+        Some(CargoSpecPartial {
+          name: "test-cargo-ssl".to_owned(),
+          container: bollard_next::container::Config {
+            image: Some(
+              "ghcr.io/next-hat/nanocl-get-started:latest".to_owned(),
+            ),
+            ..Default::default()
+          },
+          secrets: Some(vec!["test-secret-ssl".to_owned()]),
+          ..Default::default()
+        }),
+        None::<String>,
+      )
+      .await;
+    test_status_code!(res.status(), http::StatusCode::CREATED, "create cargo");
+    let cargo = TestClient::res_json::<Cargo>(res).await;
+    assert_eq!(cargo.spec.name, "test-cargo-ssl", "Invalid cargo name");
+    assert_eq!(
+      cargo.spec.container.image,
+      Some("ghcr.io/next-hat/nanocl-get-started:latest".to_owned())
+    );
+    // start the cargo
+    let res = client
+      .send_post(
+        "/processes/cargo/test-cargo-ssl/start",
+        None::<String>,
+        None::<String>,
+      )
+      .await;
+    assert_eq!(res.status(), http::StatusCode::ACCEPTED, "start cargo");
+    // Watch event for the cargo to be sure it started
+    let res = client
+      .send_post(
+        "/events/watch",
+        Some(vec![EventCondition {
+          actor_key: Some("test-cargo-ssl.global".to_owned()),
+          actor_kind: Some(EventActorKind::Cargo),
+          related_key: None,
+          related_kind: None,
+          kind: vec![EventKind::Normal],
+          action: vec![NativeEventAction::Start],
+        }]),
+        None::<String>,
+      )
+      .await;
+    assert_eq!(res.status(), http::StatusCode::OK, "watch cargo");
+    let mut stream = res.into_stream();
+    while let Some(_chunk) = stream.next().await {}
+    // Delete the cargo
+    let res = client
+      .send_delete(
+        &format!("{ENDPOINT}/test-cargo-ssl"),
+        Some(CargoDeleteQuery {
+          force: Some(true),
+          ..Default::default()
+        }),
+      )
+      .await;
+    assert_eq!(res.status(), http::StatusCode::ACCEPTED, "delete cargo");
+    // delete the secret
+    let res = client
+      .send_delete("/secrets/test-secret-ssl", None::<String>)
+      .await;
+    assert_eq!(res.status(), http::StatusCode::ACCEPTED, "delete secret");
     ntex::time::sleep(std::time::Duration::from_secs(1)).await;
     system.state.wait_event_loop().await;
   }
